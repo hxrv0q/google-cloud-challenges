dd91ea5b15b9441f487e31fa8d60cb4ee0533928
seshotake <seshotake@gmail.com>
2023-07-03 15:13:27 +0300

    Initial Infrastructure as Code With Terraform

---
+++ infrastructure-as-code-with-terraform/main.tf
@@ -0,0 +1,9 @@
+provider "google" {
+  project = var.project
+  region = "us-central1"
+  zone = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
